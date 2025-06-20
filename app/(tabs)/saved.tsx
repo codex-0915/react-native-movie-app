77e9ef6a837c6bc251406363e9eab5572a057e0c
Kyl Justin Perez <perezkyljustin@gmail.com>
2025-06-20 19:28:38 +0800

    Create the basic starter basic tabs for the app

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const saved = () => {
+  return (
+    <View>
+      <Text>Saved</Text>
+    </View>
+  )
+}
+
+export default saved
